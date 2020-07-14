7ed7ae2993675250166ae080f00934b845dd1352
nihelboujelben <nihelboujelben@gmail.com>
2020-07-14 12:43:56 +0100

    Updates

---
+++ precache-manifest.e53e865fe2c93019818624785335d5db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8e0076717eae22aa88b3cba9d8c12d",
+    "url": "/checkpoint-jsx/index.html"
+  },
+  {
+    "revision": "3262f752d33024d04e49",
+    "url": "/checkpoint-jsx/static/css/main.e0c27249.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/checkpoint-jsx/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/checkpoint-jsx/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3262f752d33024d04e49",
+    "url": "/checkpoint-jsx/static/js/main.b25d5e46.chunk.js"
+  },
+  {
+    "revision": "ff873e5233a3be13df75",
+    "url": "/checkpoint-jsx/static/js/runtime-main.5d881666.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/checkpoint-jsx/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0ddd7ca2139eb610b51ac0cd72c091b2",
+    "url": "/checkpoint-jsx/static/media/photo4.0ddd7ca2.jpg"
+  }
+]);
